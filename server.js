4137ec2601fcffb7a2e666020c5c296287ca9d41
Ubuntu <ubuntu@ip-172-31-17-164.us-east-2.compute.internal>
2019-01-05 02:20:16 +0000

    changes

--- server.js
+++ server.js
@@ -6,26 +6,30 @@ const app = express();
 app.use(express.static(__dirname));
 // app.use(cors());
 
-app.use((req, res, next) => {
+app.use((req, res, next)=> {
   let url = req.url.split("/");
   let path = url.slice(2).join("/");
   let endpoint = url[1];
   if (endpoint === "main") {
-    let proxy = "http://localhost:9001/" + path;
-    request(proxy).pipe(res);
-    // console.log(proxy);
-  } else if (endpoint === "details") {
-    let proxy = "http://localhost:9002/" + path;
-    // console.log(proxy);
-    request(proxy).pipe(res);
-  } else if (endpoint === "critics") {
-    let proxy = "http://localhost:9003/" + path;
-    console.log(proxy);
-    request(proxy).pipe(res);
-  } else if (endpoint === "sidebar") {
-    let proxy = "http://localhost:9004/" + path;
-    request(proxy).pipe(res);
+    let proxy = "http://ec2-17-128-193.us-east-2.compute.amazonaws.com/" + path;
+	console.log(proxy);  
+    request.get(proxy).on('error', function(err){
+	console.log(err);
+    })
+    .pipe(res);
   }
+
+ else if (endpoint === "details") {
+    let proxy = "http://ec2-18-218-63-15.us-east-2.compute.amazonaws.com/" + path;
+    req.pipe(request(proxy)).pipe(res);
+  } else if (endpoint === "critics") {
+    let proxy = "http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/" + path;
+    req.pipe(request(proxy)).pipe(res);
+  } else if (endpoint === "sidebar") {
+    let proxy = "http://18.222.207.221:9004/" + path;
+    req.pipe(request(proxy)).pipe(res);
+  }
+
 });
 
 app.get("/", (req, res) => {});
