2e6acc22363e6ec2940f822a5e56c98ed9ede017
Nick <nbisigna@gmail.com>
2019-01-05 09:47:32 -0500

    added code

--- server.js
+++ server.js
@@ -14,29 +14,40 @@ app.use((req, res, next) => {
   );
   next();
 });
-/*
+
 app.use((req, res, next) => {
   let url = req.url.split("/");
   let path = url.slice(2).join("/");
+  console.log(path);
   let endpoint = url[1];
   if (endpoint === "main") {
-    let proxy = "http://ec2-17-128-193.us-east-2.compute.amazonaws.com/" + path;
-    console.log(proxy);
-    request(proxy).pipe(res);
+    let proxy =
+      "http://ec2-3-17-128-193.us-east-2.compute.amazonaws.com/" + path;
+    request(proxy)
+      .on("error", err => console.log(err))
+      .pipe(res);
+    // console.log(proxy);
   } else if (endpoint === "details") {
     let proxy =
       "http://ec2-18-218-63-15.us-east-2.compute.amazonaws.com/" + path;
-    req.pipe(request(proxy)).pipe(res);
+    console.log(proxy);
+    request(proxy)
+      .on("error", err => console.log(err))
+      .pipe(res);
   } else if (endpoint === "critics") {
     let proxy =
       "http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/" + path;
-    req.pipe(request(proxy)).pipe(res);
+    // console.log(proxy);
+    request(proxy)
+      .on("error", err => console.log(err))
+      .pipe(res);
   } else if (endpoint === "sidebar") {
     let proxy = "http://18.222.207.221:9004/" + path;
-    req.pipe(request(proxy)).pipe(res);
+    request(proxy)
+      .on("error", err => console.log(err))
+      .pipe(res);
   }
 });
-*/
 app.get("/", (req, res) => {});
 
 app.listen(process.env.PORT || 3000);
