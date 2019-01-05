4853d4c6094d6ac334e4e496981fa64f9a7831d9
Nick <nbisigna@gmail.com>
2019-01-05 09:24:38 -0500

    headers

--- server.js
+++ server.js
@@ -6,27 +6,36 @@ const cors = require("cors");
 app.use(express.static(__dirname));
 app.use(cors());
 
- app.use((req, res, next) => {
-   let url = req.url.split("/");
-   let path = url.slice(2).join("/");
-   let endpoint = url[1];
-   if (endpoint === "main") {
-     let proxy = "http://ec2-17-128-193.us-east-2.compute.amazonaws.com/" + path;
-     console.log(proxy);
-      request(proxy).pipe(res);
-   } else if (endpoint === "details") {
-     let proxy =
-       "http://ec2-18-218-63-15.us-east-2.compute.amazonaws.com/" + path;
-     req.pipe(request(proxy)).pipe(res);
-   } else if (endpoint === "critics") {
-     let proxy =
-       "http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/" + path;
-     req.pipe(request(proxy)).pipe(res);
-   } else if (endpoint === "sidebar") {
-     let proxy = "http://18.222.207.221:9004/" + path;
-     req.pipe(request(proxy)).pipe(res);
-   }
- });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use((req, res, next) => {
+  let url = req.url.split("/");
+  let path = url.slice(2).join("/");
+  let endpoint = url[1];
+  if (endpoint === "main") {
+    let proxy = "http://ec2-17-128-193.us-east-2.compute.amazonaws.com/" + path;
+    console.log(proxy);
+    request(proxy).pipe(res);
+  } else if (endpoint === "details") {
+    let proxy =
+      "http://ec2-18-218-63-15.us-east-2.compute.amazonaws.com/" + path;
+    req.pipe(request(proxy)).pipe(res);
+  } else if (endpoint === "critics") {
+    let proxy =
+      "http://ec2-3-16-138-173.us-east-2.compute.amazonaws.com/" + path;
+    req.pipe(request(proxy)).pipe(res);
+  } else if (endpoint === "sidebar") {
+    let proxy = "http://18.222.207.221:9004/" + path;
+    req.pipe(request(proxy)).pipe(res);
+  }
+});
 
 app.get("/", (req, res) => {});
 
