6dd42c594434f979710e4db51af69d9aaae915a9
paul lee <paul.plk@gmail.com>
2019-01-16 14:17:59 -0500

    changed proxy

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const path = require('path');
 const proxy = require('http-proxy-middleware');
 
 
@@ -14,19 +13,19 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-app.use('/:id', express.static(path.join(__dirname, 'public')));
+app.use('/:movieId', express.static(__dirname));
 
-app.use('/api/items', proxy({
-    target: 'http://ec2-18-223-255-148.us-east-2.compute.amazonaws.com:3005'
+app.use('/movie', proxy({
+    target: 'http://ec2-18-218-43-35.us-east-2.compute.amazonaws.com:9008'
 }));
 
-app.use('/api/pictures', proxy({
-    target: 'http://ec2-3-17-163-197.us-east-2.compute.amazonaws.com:5000'
-}));
+// app.use('/api/pictures', proxy({
+//     target: 'http://ec2-3-17-163-197.us-east-2.compute.amazonaws.com:5000'
+// }));
 
-app.use('/api/booking', proxy({
-    target: 'http://ec2-18-222-217-73.us-east-2.compute.amazonaws.com:3008'
-}))
+// app.use('/api/booking', proxy({
+//     target: 'http://ec2-18-222-217-73.us-east-2.compute.amazonaws.com:3008'
+// }))
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
