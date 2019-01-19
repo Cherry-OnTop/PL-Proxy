cef1124ca9efcc3fff0e6d4aed4791e85b5b9586
paul lee <paul.plk@gmail.com>
2019-01-19 12:13:43 -0500

    made changes to let my service work for team members proxies

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ app.use(bodyParser.urlencoded({
 app.use('/:movieId', express.static(__dirname));
 
 app.use('/movie', proxy({
-    target: 'http://ec2-3-17-32-68.us-east-2.compute.amazonaws.com:9008'
+    target: 'http://18.217.66.137'
 }));
 
-// app.use('/api/pictures', proxy({
-//     target: 'http://ec2-3-17-163-197.us-east-2.compute.amazonaws.com:5000'
-// }));
+app.use('/movies', proxy({
+    target: 'http://54.146.26.33'
+}));
 
 // app.use('/api/booking', proxy({
 //     target: 'http://ec2-18-222-217-73.us-east-2.compute.amazonaws.com:3008'
